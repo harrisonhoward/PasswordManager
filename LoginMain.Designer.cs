148fe1865b0347d0d2f25f9aeaef6b0a6f51eb86
Forbidden-Duck <forbiddenducky@gmail.com>
2020-04-09 12:38:54 +1000

    Merged LoginMenu and NewUser together into 1 form called LoginMain.cs

---
+++ LoginMain.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace PasswordManager {
+    partial class frmLogin {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panLogin = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cbSignedIn = new System.Windows.Forms.CheckBox();
+            this.lnkNewUser = new System.Windows.Forms.LinkLabel();
+            this.txtMainPassword = new System.Windows.Forms.TextBox();
+            this.txtMainUsername = new System.Windows.Forms.TextBox();
+            this.lblMainPassword = new System.Windows.Forms.Label();
+            this.lblMainUsername = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblMainTitle = new System.Windows.Forms.Label();
+            this.panCreate = new System.Windows.Forms.Panel();
+            this.txtConfirm = new System.Windows.Forms.TextBox();
+            this.lblConfirm = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblCreate = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panLogin.SuspendLayout();
+            this.panCreate.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panLogin
+            // 
+            this.panLogin.Controls.Add(this.btnExit);
+            this.panLogin.Controls.Add(this.cbSignedIn);
+            this.panLogin.Controls.Add(this.lnkNewUser);
+            this.panLogin.Controls.Add(this.txtMainPassword);
+            this.panLogin.Controls.Add(this.txtMainUsername);
+            this.panLogin.Controls.Add(this.lblMainPassword);
+            this.panLogin.Controls.Add(this.lblMainUsername);
+            this.panLogin.Controls.Add(this.btnLogin);
+            this.panLogin.Controls.Add(this.lblMainTitle);
+            this.panLogin.Location = new System.Drawing.Point(0, 0);
+            this.panLogin.Name = "panLogin";
+            this.panLogin.Size = new System.Drawing.Size(344, 342);
+            this.panLogin.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(275, 311);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(66, 28);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // cbSignedIn
+            // 
+            this.cbSignedIn.AutoSize = true;
+            this.cbSignedIn.Checked = global::PasswordManager.Properties.Settings.Default.StaySignedIn;
+            this.cbSignedIn.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::PasswordManager.Properties.Settings.Default, "StaySignedIn", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.cbSignedIn.Location = new System.Drawing.Point(182, 204);
+            this.cbSignedIn.Name = "cbSignedIn";
+            this.cbSignedIn.Size = new System.Drawing.Size(95, 17);
+            this.cbSignedIn.TabIndex = 16;
+            this.cbSignedIn.Text = "Stay Signed In";
+            this.cbSignedIn.UseVisualStyleBackColor = true;
+            // 
+            // lnkNewUser
+            // 
+            this.lnkNewUser.AutoSize = true;
+            this.lnkNewUser.Location = new System.Drawing.Point(41, 205);
+            this.lnkNewUser.Name = "lnkNewUser";
+            this.lnkNewUser.Size = new System.Drawing.Size(112, 13);
+            this.lnkNewUser.TabIndex = 15;
+            this.lnkNewUser.TabStop = true;
+            this.lnkNewUser.Text = "Create a new account";
+            this.lnkNewUser.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LnkNewUser_LinkClicked);
+            // 
+            // txtMainPassword
+            // 
+            this.txtMainPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMainPassword.Location = new System.Drawing.Point(122, 172);
+            this.txtMainPassword.MaxLength = 60;
+            this.txtMainPassword.Name = "txtMainPassword";
+            this.txtMainPassword.Size = new System.Drawing.Size(155, 22);
+            this.txtMainPassword.TabIndex = 14;
+            this.txtMainPassword.UseSystemPasswordChar = true;
+            // 
+            // txtMainUsername
+            // 
+            this.txtMainUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMainUsername.Location = new System.Drawing.Point(122, 116);
+            this.txtMainUsername.MaxLength = 60;
+            this.txtMainUsername.Name = "txtMainUsername";
+            this.txtMainUsername.Size = new System.Drawing.Size(155, 22);
+            this.txtMainUsername.TabIndex = 13;
+            // 
+            // lblMainPassword
+            // 
+            this.lblMainPassword.AutoSize = true;
+            this.lblMainPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainPassword.Location = new System.Drawing.Point(41, 173);
+            this.lblMainPassword.Name = "lblMainPassword";
+            this.lblMainPassword.Size = new System.Drawing.Size(75, 18);
+            this.lblMainPassword.TabIndex = 12;
+            this.lblMainPassword.Text = "Password";
+            // 
+            // lblMainUsername
+            // 
+            this.lblMainUsername.AutoSize = true;
+            this.lblMainUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainUsername.Location = new System.Drawing.Point(39, 117);
+            this.lblMainUsername.Name = "lblMainUsername";
+            this.lblMainUsername.Size = new System.Drawing.Size(77, 18);
+            this.lblMainUsername.TabIndex = 11;
+            this.lblMainUsername.Text = "Username";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(119, 288);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 40);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // lblMainTitle
+            // 
+            this.lblMainTitle.AutoSize = true;
+            this.lblMainTitle.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainTitle.Location = new System.Drawing.Point(64, 38);
+            this.lblMainTitle.Name = "lblMainTitle";
+            this.lblMainTitle.Size = new System.Drawing.Size(213, 25);
+            this.lblMainTitle.TabIndex = 9;
+            this.lblMainTitle.Text = "Password Manager";
+            // 
+            // panCreate
+            // 
+            this.panCreate.Controls.Add(this.btnClose);
+            this.panCreate.Controls.Add(this.txtConfirm);
+            this.panCreate.Controls.Add(this.lblConfirm);
+            this.panCreate.Controls.Add(this.txtPassword);
+            this.panCreate.Controls.Add(this.txtUsername);
+            this.panCreate.Controls.Add(this.lblPassword);
+            this.panCreate.Controls.Add(this.lblUsername);
+            this.panCreate.Controls.Add(this.btnCreate);
+            this.panCreate.Controls.Add(this.lblCreate);
+            this.panCreate.Location = new System.Drawing.Point(0, 0);
+            this.panCreate.Name = "panCreate";
+            this.panCreate.Size = new System.Drawing.Size(344, 342);
+            this.panCreate.TabIndex = 18;
+            // 
+            // txtConfirm
+            // 
+            this.txtConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirm.Location = new System.Drawing.Point(148, 200);
+            this.txtConfirm.MaxLength = 60;
+            this.txtConfirm.Name = "txtConfirm";
+            this.txtConfirm.Size = new System.Drawing.Size(155, 22);
+            this.txtConfirm.TabIndex = 22;
+            this.txtConfirm.UseSystemPasswordChar = true;
+            // 
+            // lblConfirm
+            // 
+            this.lblConfirm.AutoSize = true;
+            this.lblConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirm.Location = new System.Drawing.Point(10, 201);
+            this.lblConfirm.Name = "lblConfirm";
+            this.lblConfirm.Size = new System.Drawing.Size(132, 18);
+            this.lblConfirm.TabIndex = 21;
+            this.lblConfirm.Text = "Confirm Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(148, 172);
+            this.txtPassword.MaxLength = 60;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(155, 22);
+            this.txtPassword.TabIndex = 20;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(148, 116);
+            this.txtUsername.MaxLength = 60;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(155, 22);
+            this.txtUsername.TabIndex = 19;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(67, 173);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(75, 18);
+            this.lblPassword.TabIndex = 18;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(65, 117);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(77, 18);
+            this.lblUsername.TabIndex = 17;
+            this.lblUsername.Text = "Username";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(115, 288);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(123, 40);
+            this.btnCreate.TabIndex = 16;
+            this.btnCreate.Text = "Create Account";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.BtnCreate_Click);
+            // 
+            // lblCreate
+            // 
+            this.lblCreate.AutoSize = true;
+            this.lblCreate.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCreate.Location = new System.Drawing.Point(80, 38);
+            this.lblCreate.Name = "lblCreate";
+            this.lblCreate.Size = new System.Drawing.Size(191, 25);
+            this.lblCreate.TabIndex = 15;
+            this.lblCreate.Text = "Account Creation";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(275, 311);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(66, 28);
+            this.btnClose.TabIndex = 23;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 343);
+            this.Controls.Add(this.panCreate);
+            this.Controls.Add(this.panLogin);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginMain";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmLogin_FormClosing);
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.panLogin.ResumeLayout(false);
+            this.panLogin.PerformLayout();
+            this.panCreate.ResumeLayout(false);
+            this.panCreate.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox cbSignedIn;
+        private System.Windows.Forms.LinkLabel lnkNewUser;
+        private System.Windows.Forms.TextBox txtMainPassword;
+        private System.Windows.Forms.TextBox txtMainUsername;
+        private System.Windows.Forms.Label lblMainPassword;
+        private System.Windows.Forms.Label lblMainUsername;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblMainTitle;
+        private System.Windows.Forms.Panel panCreate;
+        private System.Windows.Forms.TextBox txtConfirm;
+        private System.Windows.Forms.Label lblConfirm;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lblCreate;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ LoginMain.cs
@@ -0,0 +1,293 @@
+﻿using PasswordEncryptor;
+using SQLController;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordManager {
+    public partial class frmLogin : Form {
+        #region Global Variables
+
+        // Create a variable for User DataTable
+        // Create variables for User Input for Username and Password
+        // Create a variable for the UserID
+        /* 
+         * Create a variable for checking the User DataTable
+         * contains the User Input Username
+        */
+        DataTable _userTable;
+        string _usersUsername = "", _usersPassword = "";
+        long _userID = 0;
+        bool _tableContains = false;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of our form
+        /// </summary>
+        public frmLogin() {
+            // Initializes the form components
+            InitializeComponent();
+            // Hides the Create Panel
+            panCreate.Hide();
+            Text = "Login";
+        }
+
+        #endregion
+
+        #region Form Events
+
+        private void FrmLogin_Load(object sender, EventArgs e) {
+            // Create and assign LoginDetails DataTable
+            DataTable loginTable = Context.GetDataTable("LoginDetails");
+
+            // If the DataTable contains rows
+            // Login using the UserID
+            if (loginTable.Rows.Count > 0) {
+                _userID = long.Parse(loginTable.Rows[0]["UserID"].ToString());
+                ThreadStart(new System.Threading.Thread(new System.Threading.ThreadStart(ThreadProcPasswordList)));
+            }
+        }
+
+        private void FrmLogin_FormClosing(object sender, FormClosingEventArgs e) {
+            // Check if Stay Signed In is check and a userID has been set
+            if (cbSignedIn.Checked && _userID != 0) {
+                // Create and assign LoginDetails DataTable
+                DataTable loginTable = Context.GetDataTable("LoginDetails");
+
+                // Create a new row in the DataTable
+                // Change the UserID to the saved UserID
+                // Save the row
+                // Add the row
+                DataRow row = loginTable.NewRow();
+                row["UserID"] = _userID;
+                row.EndEdit();
+                loginTable.Rows.Add(row);
+
+                // Save the table
+                Context.SaveDataBaseTable(loginTable);
+            }
+        }
+
+        #endregion
+
+        #region Button Events
+
+        private void BtnLogin_Click(object sender, EventArgs e) {
+            // Checking if the user has inputted values
+            // Showing MessageBox providing reason
+            if (string.IsNullOrEmpty(txtMainUsername.Text)) {
+                MessageBox.Show("Please enter a username",
+                    Properties.Settings.Default.ProjectName,
+                    MessageBoxButtons.OK);
+                return;
+            } else if (string.IsNullOrEmpty(txtMainPassword.Text)) {
+                MessageBox.Show("Please enter a password",
+                    Properties.Settings.Default.ProjectName,
+                    MessageBoxButtons.OK);
+                return;
+            }
+
+            // Assign the User Inputted Username and Password to the Global Variables
+            _usersUsername = txtMainUsername.Text;
+            _usersPassword = txtMainPassword.Text;
+
+            // Initialize the User DataTable
+            InitializeUserTable(false);
+
+            // Check if the username exists
+            // Show a MessageBox
+            if (_userTable.Rows.Count < 1) {
+                MessageBox.Show($"No user by the username '{_usersUsername}' (case sensitive) was found",
+                    Properties.Settings.Default.ProjectName,
+                    MessageBoxButtons.OK);
+                return;
+            }
+
+            // Creating a string for the password hash
+            string userPasswordHash = _userTable.Rows[0]["PasswordHash"].ToString();
+
+            // Comparing the users input to the hash password
+            bool isEquals = HashSalt.CompareInputtoPassword(_usersPassword, userPasswordHash);
+
+            // Checking if isEquals is true
+            // Assign userID and starting PasswordList (with UserID)
+            // Else Showing a MessageBox and resetting the variables
+            if (isEquals) {
+                _userID = long.Parse(_userTable.Rows[0]["UserID"].ToString());
+                ThreadStart(new System.Threading.Thread(new System.Threading.ThreadStart(ThreadProcPasswordList)));
+            } else {
+                MessageBox.Show("Password was incorrect",
+                    Properties.Settings.Default.ProjectName,
+                    MessageBoxButtons.OK);
+                _usersUsername = "";
+                _usersPassword = "";
+                _userTable = null;
+                return;
+            }
+        }
+
+        private void BtnCreate_Click(object sender, EventArgs e) {
+            // Checking if the user has inputted values
+            // Showing a MessageBox with the provided reason
+            if (string.IsNullOrEmpty(txtUsername.Text)) {
+                MessageBox.Show("Please enter a username",
+                    Properties.Settings.Default.ProjectName,
+                    MessageBoxButtons.OK);
+                return;
+            } else if (string.IsNullOrEmpty(txtPassword.Text)) {
+                MessageBox.Show("Please enter a password",
+                    Properties.Settings.Default.ProjectName,
+                    MessageBoxButtons.OK);
+                return;
+            }
+
+            // Comparing the Password with Confirm Password
+            // If not show a MessageBox
+            if (!txtPassword.Text.Equals(txtConfirm.Text)) {
+                MessageBox.Show("Confirm Password does not match Password",
+                    Properties.Settings.Default.ProjectName,
+                    MessageBoxButtons.OK);
+                return;
+            }
+
+            // Assign the User Inputted Username and Password to the Global Variables
+            _usersUsername = txtUsername.Text;
+            _usersPassword = txtPassword.Text;
+
+            // Initialize the User DataTable
+            InitializeUserTable(true);
+
+            // Check if the username exists
+            // Show a MessageBox
+            if (_tableContains) {
+                MessageBox.Show($"A user by the username '{_usersUsername}' already exists",
+                    Properties.Settings.Default.ProjectName,
+                    MessageBoxButtons.OK);
+                return;
+            }
+
+            // Converting the inputted password to a Hash Salt
+            _usersPassword = HashSalt.StringtoHashSalt(_usersPassword);
+
+            // Adding a new row to User DataTable
+            // Inputted the new data for the row before adding it
+            DataRow row = _userTable.NewRow();
+            row["UserID"] = _userTable.Rows.Count + 1;
+            row["Username"] = _usersUsername;
+            row["PasswordHash"] = _usersPassword;
+            _userTable.Rows.Add(row);
+
+
+            // Create and assign the column names
+            string columnNames = "UserID, Username, PasswordHash";
+
+            // Create and assign the column values
+            string columnValues =
+                $"{row["UserID"]}, " +
+                $"'{row["Username"]}', " +
+                $"'{row["PasswordHash"]}'";
+
+            // Insert the record
+            Context.InsertRecord("Users", columnNames, columnValues);
+
+            // Show the Login Panel
+            panLogin.Show();
+            Text = "Login";
+
+
+            // Hide the Create Panel
+            panCreate.Hide();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e) {
+            // Closes the form
+            Close();
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e) {
+            // Show the Login Panel
+            panLogin.Show();
+            Text = "Login";
+
+
+            // Hide the Create Panel
+            panCreate.Hide();
+        }
+
+        #endregion
+
+        #region LinkLabel Events
+
+        private void LnkNewUser_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            // Hide the Login Panel
+            panLogin.Hide();
+            
+            // Show the Create Panel
+            panCreate.Show();
+            Text = "Create an account";
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Runs a form
+        /// </summary>
+        private void ThreadProcPasswordList() {
+            Application.Run(new frmPasswordList(_userID));
+        }
+
+        /// <summary>
+        /// Starts a new thread
+        /// </summary>
+        /// <param name="thread">The new thread</param>
+        private void ThreadStart(System.Threading.Thread thread) {
+            // Start the new thread
+            // Close the current form
+            thread.Start();
+            Close();
+        }
+
+        /// <summary>
+        /// Initializes the User DataTable
+        /// </summary>
+        private void InitializeUserTable(bool isCreate) {
+            // If isCreate = true
+            // Create a editable DataTable
+            // Else Get the a single user
+            if (isCreate) {
+                // Create and assign a new SQL Query
+                // Assign the User DataTable with the User Table
+                _userTable = Context.GetDataTable("Users");
+
+                // For each row in the User DataTable
+                // If the Username column in the row is equals to the User Input Username
+                // Set the tableContains global variable to true
+                foreach (DataRow row in _userTable.Rows) {
+                    if (row["Username"].Equals(_usersUsername)) {
+                        _tableContains = true;
+                    }
+                }
+            } else {
+                // Create and assign a new SQL Query
+                // Assign the User DataTable with the User Table
+                string sqlQuery =
+                    "SELECT * FROM Users " +
+                    $"WHERE Username='{_usersUsername}'";
+                _userTable = Context.GetDataTable(sqlQuery, "Users", true);
+            }
+        }
+
+        #endregion
+    }
+}
