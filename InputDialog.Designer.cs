3888c5834a1bd14f0bd40ffd16a1c1dc562b838d
Forbidden-Duck <forbiddenducky@gmail.com>
2020-04-13 15:30:47 +1000

    Removed icon on the input dialog

--- InputDialog.Designer.cs
+++ InputDialog.Designer.cs
@@ -81,6 +81,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "InputDialog";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Title";
             this.ResumeLayout(false);
